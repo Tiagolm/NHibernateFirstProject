8fadbe6702eb23daa30266ac704856dc72473293
Tiago Moura <tiagolopesdemoura@gmail.com>
2023-11-15 09:37:48 -0300

    Add project files.

---
+++ NHibernateFirstProject/Mappings/EmployeeMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using NHibernateFirstProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateFirstProject.Mappings
+{
+    public class EmployeeMap : ClassMap<Employee>
+    {
+        public EmployeeMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+            References(x => x.Store);
+        }
+    }
+}
